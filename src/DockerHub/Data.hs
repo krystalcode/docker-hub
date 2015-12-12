7f05b2c2d540a66350046b2a745a33192901300d
Dimitris Bozelos <dbozelos@gmail.com>
2015-12-12 23:59:56 +0000

    added Repository data type

---
+++ src/DockerHub/Data.hs
@@ -0,0 +1,33 @@
+{-# LANGUAGE OverloadedStrings, DeriveGeneric #-}
+
+module DockerHub.Data
+       ( Repository(..)
+       ) where
+
+{-
+  @Issue(
+    "Check if we could limit what is imported from Aeson and Generics"
+    type="improvement"
+    priority="low"
+  )
+-}
+import Data.Aeson
+import Data.Aeson.Types (typeMismatch)
+import GHC.Generics
+
+-- Public API/types.
+
+-- Data structure that holds information about a DockerHub repository.
+data Repository = Repository { name :: String
+                             , triggerToken :: String } deriving (Show, Generic)
+
+-- Functions/types for internal use.
+
+-- Conversion from and to JSON.
+instance FromJSON Repository where
+    parseJSON (Object v) = Repository <$>
+                           v .: "name" <*>
+                           v .: "trigger_token"
+    parseJSON invalid    = typeMismatch "Repository" invalid
+
+instance ToJSON Repository
