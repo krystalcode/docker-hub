e418f3dd4099aee8b8c88c720895d02cd08909e2
Dimitris Bozelos <dbozelos@gmail.com>
2015-12-13 00:05:40 +0000

    added functionality for triggering builds

---
+++ src/DockerHub/Build.hs
@@ -0,0 +1,20 @@
+module DockerHub.Build
+       ( build ) where
+
+import DockerHub.Data
+import Network.Curl (curlPost)
+
+-- Public API/types.
+
+-- Trigger build for a list of repositories.
+build :: [Repository] -> IO [()]
+build repositories = mapM build' repositories
+
+-- Functions/types for internal use.
+
+-- Trigger build for an individual repository.
+build' :: Repository -> IO ()
+build' repository = curlPost requestUrl []
+                    where requestUrl = "https://registry.hub.docker.com/u/" ++ repoName ++ "/trigger/" ++ repoTriggerToken ++ "/"
+                          repoName = name repository
+                          repoTriggerToken = triggerToken repository
